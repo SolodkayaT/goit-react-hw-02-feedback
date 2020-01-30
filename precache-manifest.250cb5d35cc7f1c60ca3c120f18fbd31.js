49f127ff6aa254661235f44f8b1875a05ff9d378
SolodkayaT <tatasladkaya@gmail.com>
2020-01-30 14:31:34 +0200

    Updates

---
+++ precache-manifest.250cb5d35cc7f1c60ca3c120f18fbd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9593c46f693e8c0c3ea7809f774327a0",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "33be80dc764163be53ca",
+    "url": "/goit-react-hw-02-feedback/static/css/main.13c21db1.chunk.css"
+  },
+  {
+    "revision": "040fe006b4ae28abdf45",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0cfd4b2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0cfd4b2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33be80dc764163be53ca",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1c781166.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
